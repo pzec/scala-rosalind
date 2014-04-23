f107191c43ce2be0d95e658ad088f3d3afd5c0dc
Pedja <zekaleon@gmail.com>
2014-04-23 18:44:01 +0200

    first commit

---
+++ rcomplement.scala
@@ -0,0 +1,17 @@
+package scala_one
+import scala.io.Source
+
+
+object rcomplement {
+  //val s:String = "AAAACCCGGT"
+  val complements = Map('A'->'T', 'C'->'G', 'G'->'C', 'T'->'A')
+  val f = Source.fromFile("/home/pedja/Downloads/rosalind_revc.txt")
+  val s = f.getLines.next
+  def rcomplement(s:String, complements:Map[Char, Char]):List[Char] = (s.reverse.toCharArray map(c => complements(c))).toList
+  
+  def main(args: Array[String]){
+    println(rcomplement(s, complements).mkString)
+  }
+
+}
+
---
+++ s_one.scala
@@ -0,0 +1,24 @@
+package scala_one
+import scala.BigInt
+
+class A(apar:Int){
+  var a:Int = apar
+}
+
+object B{
+  val x=5
+}
+
+object s_one {
+  
+  def fact(x: BigInt):BigInt = {
+    if (x==0) 1 else x*fact(x-1)
+  }
+  
+  def main(args: Array[String]){
+    println(fact(30))    
+    val c = new A(3)
+    val d = B
+  }
+
+}
