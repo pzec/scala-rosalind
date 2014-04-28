9432fca0c3478c5706a6fec64b3f3e5d77fcf258
Pedja <predrag.zecevic@sbgenomics.com>
2014-04-28 17:46:31 +0200

    mortal rabbits solved

--- rabbits.scala
+++ rabbits.scala
@@ -20,32 +20,53 @@ object rabbits {
     dyna(x.toInt-1)
   }
   
+  //mortal fibonacci rabbits
+  //x-th generation if every rabbit lives k generations, and every rabbit makes another rabbit each month 
   def fibo3(x:BigInt, k:Int):BigInt = {
     var dyna = List[BigInt]()
     var newborn = List[BigInt]()
+    var oldtimers = List[BigInt]()
     //first month
     dyna = dyna ::: List(BigInt(1))
     newborn = newborn ::: List(BigInt(1))
+    oldtimers = oldtimers ::: List(BigInt(0))
     //second month
     dyna = dyna ::: List(BigInt(1))
     newborn = newborn ::: List(BigInt(0))
-    
-    for (i:BigInt <- BigInt(3) to x+1){
-      val y:BigInt = if (i.toInt-k > 0) (newborn(i.toInt-k)) else 0
-      dyna = dyna ::: List(dyna(i.toInt-2) + dyna(i.toInt-3) - y)
-      newborn = newborn ::: List(dyna(i.toInt-3))
+    oldtimers = oldtimers ::: List(BigInt(0))
+    //println((BigInt(3) to k-1).toList)
+    for (i:BigInt <- BigInt(3) to k-1){
+      oldtimers = oldtimers ::: List(BigInt(0))
+      dyna = dyna ::: List((dyna(i.toInt-2) - newborn(i.toInt-2))* 2 + newborn(i.toInt-2))
+      newborn = newborn ::: List(dyna(i.toInt-2)-newborn(i.toInt-2))
     }
     
+    for (i:BigInt <- BigInt(k) to x+1){
+      //val y:BigInt = if (i.toInt-k > 0) (newborn(i.toInt-k)) else 0
+      
+      newborn = newborn ::: List(dyna(i.toInt-2)-newborn(i.toInt-2))
+      oldtimers = oldtimers ::: List(newborn(i.toInt-k))
+      dyna = dyna ::: List(2*(dyna(i.toInt-2) - oldtimers(i.toInt-2) - newborn(i.toInt-2)) + newborn(i.toInt-2) + oldtimers(i.toInt-2)) 
+      
+      //println(i + " " + dyna(i.toInt-2) + " " + newborn(i.toInt-2) + " " + oldtimers(i.toInt-2))
+    }
+    
+//    println(dyna)
+//    println(newborn)
+//    println(oldtimers)
     dyna(x.toInt-1)
   }
   
   def main(args: Array[String]) {
-    val f = Source.fromFile("C:/Users/pedja/Downloads/rosalind_fibd.txt")
+    //val f = Source.fromFile("C:/Users/pedja/Downloads/rosalind_fibd.txt")
+    val f = Source.fromFile("/home/pedja/Downloads/rosalind_fibd.txt")
     val (n,k) = f.getLines.next.split(' ') match {case Array(a:String, b:String) => (a.toInt, b.toInt)}
-    println(fibo(5))
-    //println(fibo2(n, k))
-    
-    println(fibo3(6, 3))
+    //println(fibo(5))
+    println(fibo3(n, k))
+//    println(fibo3(7, 4))
+//    println(fibo3(6, 3))
+//    println(fibo3(7, 5))
+//    println(fibo3(12, 3))
     
   }
 
